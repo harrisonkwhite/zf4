6da4ad43d89eb99e0e5b5ce5a4b6733f57897f96
Harry White <harrywhiteproductions@gmail.com>
2024-12-27 20:52:53 +1000

    Add utility macro for getting the length of a static array.

--- zf4_common/include/zf4c.h
+++ zf4_common/include/zf4c.h
@@ -5,5 +5,6 @@
 #include <zf4c_assets.h>
 #include <zf4c_mem.h>
 #include <zf4c_math.h>
+#include <zf4c_misc.h>
 
 #endif
---
+++ zf4_common/include/zf4c_misc.h
@@ -0,0 +1,6 @@
+#ifndef ZF4C_MISC_H
+#define ZF4C_MISC_H
+
+#define ZF4_STATIC_ARRAY_LEN(X) (sizeof(X) / sizeof(*(X)))
+
+#endif
