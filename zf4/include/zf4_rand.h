aa3f1d24a2bc3e59fcbfcaf7acf25a7c12404e49
Harry White <harrywhiteproductions@gmail.com>
2025-02-04 16:45:27 +1000

    Add function for generating random direction.

--- zf4/include/zf4_rand.h
+++ zf4/include/zf4_rand.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <zf4c_math.h>
+
 namespace zf4 {
     void InitRNG(void);
     int RandInt(const int min, const int max);
     float RandFloat(const float min, const float max);
     float RandPerc(void);
+
+    inline float RandDir(void) {
+        return RandFloat(0.0f, 2.0f * g_pi);
+    }
 }
